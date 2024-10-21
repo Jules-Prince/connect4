386cbfd29e504b579dd60365782647cfc10601f9
julesprince <jules.prince.fr@icloud.com>
2024-10-21 20:10:30 +0200

    ✨ First commit
    first commit hehe

---
+++ build/CMakeFiles/connect4.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/connect4.dir/src/main.cpp.o"
+  "CMakeFiles/connect4.dir/src/main.cpp.o.d"
+  "connect4"
+  "connect4.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/connect4.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/connect4.dir/src/main.cpp.o.d
@@ -0,0 +1,4 @@
+CMakeFiles/connect4.dir/src/main.cpp.o: \
+  /Users/julesprince/GIT/DEV/connect4/src/main.cpp \
+  /Users/julesprince/GIT/DEV/connect4/build/_deps/raylib-src/src/raylib.h \
+  /Library/Developer/CommandLineTools/usr/lib/clang/15.0.0/include/stdarg.h
